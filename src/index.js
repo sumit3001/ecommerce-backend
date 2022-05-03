022020335ec823d5379b3c9986a40535d8c0499b
youngmansumit <svsumitvishwakarma@gmail.com>
2022-05-03 11:17:09 +0530

    schema

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './services/models/connectDB';
 dotenv.config('./.env');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080
+
+connectDB();
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)
