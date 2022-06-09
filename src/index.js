289898bf8b57e0a8e7d321701d7976efa221813e
youngmansumit <svsumitvishwakarma@gmail.com>
2022-06-09 16:04:13 +0530

    order route

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './services/mongodb/connectDB';
 dotenv.config('./.env');
-import userRoutes from './routes/user'
-import categoryRoutes from './routes/category'
-import productRoutes from './routes/product'
-import addressRoutes from './routes/address'
+import userRoutes from './routes/user';
+import categoryRoutes from './routes/category';
+import productRoutes from './routes/product';
+import addressRoutes from './routes/address';
+import orderRoutes from './routes/order';
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use('/user', userRoutes);
 app.use('/category', categoryRoutes);
 app.use('/product', productRoutes);
 app.use('/address', addressRoutes);
+app.use('/order', orderRoutes);
 
 app.get('/', (req,res)=>{
     res.send(`server Deployed by github actions ${PORT}`)
---
+++ src/routes/order.js
@@ -0,0 +1,139 @@
+import express from "express";
+const router = express.Router();
+import { isAuthenticated } from "../services/middlewares/isAuthenticated";
+import { isAdmin } from "../services/middlewares/isAdmin";
+import { body, validationResult } from "express-validator";
+import { Order, User } from "../services/mongodb/schema";
+
+/*
+type : POST
+path : /order/create
+body : (address, user, products, total, status)
+query: none
+description : Route to create order
+*/
+
+router.post(
+  "/create",
+  isAuthenticated,
+  body("total").isNumeric(),
+  async (req, res) => {
+    try {
+      const { errors } = validationResult(req);
+      console.log(errors);
+      if (errors.length > 0)
+        // validation
+        return res.json({
+          data: {
+            category: null,
+          },
+          success: false,
+          message: "Validation failed",
+        });
+
+      const user = req.user;
+      const { address, products, total, status } = req.body;
+
+      const order = new Order({
+        address,
+        user,
+        products,
+        total,
+        status,
+      });
+      await order.save();
+
+      await User.findOneAndUpdate(
+        {
+          _id: user,
+        },
+        {
+          $addToSet: { orders: order._id },
+        }
+      );
+      // addToSet:- this add new entity to the set
+
+      return res.json({
+        data: {
+          order,
+        },
+        success: true,
+        message: "address added succesfully",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.json({
+        data: {
+          order: null,
+        },
+        success: false,
+        message: error.message,
+      });
+    }
+  }
+);
+
+/*
+type : GET
+path : /order/all
+body : none
+query: none
+description : Route to get all orders
+*/
+
+router.post("/all", isAuthenticated, isAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find({});
+
+    return res.json({
+      data: {
+        orders,
+      },
+      success: true,
+      message: "Orders fetched successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      data: {
+        orders: null,
+      },
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+/*
+type : GET
+path : /order/me
+body : none
+query: none
+description : Route to get users orders
+*/
+
+router.post("/me", isAuthenticated, async (req, res) => {
+  try {
+    const user = req.user;
+    const orders = await Order.find({ _id: user });
+
+    return res.json({
+      data: {
+        orders,
+      },
+      success: true,
+      message: "Orders fetched successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      data: {
+        orders: null,
+      },
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+export default router;
