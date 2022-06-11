2e7c7874edc44bbab637c5423032d4540f757fff
youngmansumit <svsumitvishwakarma@gmail.com>
2022-06-11 10:08:02 +0530

    delete api added in category

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,9 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { isAuthenticated } from '../services/middlewares/isAuthenticated';
-import { isAdmin } from '../services/middlewares/isAdmin';
+import { isAuthenticated } from "../services/middlewares/isAuthenticated";
+import { isAdmin } from "../services/middlewares/isAdmin";
 import { body, validationResult } from "express-validator";
-import { Category } from "../services/mongodb/schema";
+import { Category, Product } from "../services/mongodb/schema";
 
 /*
 type : POST
@@ -23,7 +23,8 @@ router.post(
     try {
       const { errors } = validationResult(req);
       console.log(errors);
-      if (errors.length > 0) // validation
+      if (errors.length > 0)
+        // validation
         return res.json({
           data: {
             category: null,
@@ -32,19 +33,18 @@ router.post(
           message: "Validation failed",
         });
 
-      const {name, description} = req.body;
-        
+      const { name, description } = req.body;
+
       console.log(req.user);
-      const category = new Category({name, description});
+      const category = new Category({ name, description });
       await category.save();
       return res.json({
         data: {
           category,
         },
         success: true,
-        message: 'category added succesfully',
+        message: "category added succesfully",
       });
-
     } catch (error) {
       console.log(error);
       return res.json({
@@ -67,28 +67,71 @@ description : Route to fetch all category
 */
 
 router.get("/all", async (req, res) => {
-    try {
-      const categories = await Category.find()
-      return res.json({
-        data: {
-          categories,
-        },
-        success: true,
-        message: 'categories fetched succesfully',
-      });
+  try {
+    const categories = await Category.find();
+    return res.json({
+      data: {
+        categories,
+      },
+      success: true,
+      message: "categories fetched succesfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      data: {
+        categories: [],
+      },
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
-    } catch (error) {
-      console.log(error);
+/*
+type : DELETE
+path : /category/:id
+body : none
+query: none
+description : Route to delete a product 
+*/
+
+router.delete("/:id", isAuthenticated, isAdmin, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // check if products using category
+
+    const products = await Product.find({ category: id });
+
+    if (products.length > 0)
       return res.json({
         data: {
-          categories: [],
+          category: null,
         },
         success: false,
-        message: error.message,
+        message: "Category could not deleted",
       });
-    }
+    const category = await Category.findOneAndDelete({ _id: id });
+
+    return res.json({
+      data: {
+        category,
+      },
+      success: true,
+      message: "Category deleted succesfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      data: {
+        category: [],
+      },
+      success: false,
+      message: error.message,
+    });
   }
-);
+});
 
 // update api remaining
 
